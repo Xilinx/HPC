cedac85df79b104c890d9ff291092d38e27e093e
Lisa Liu <email@example.com>
2021-04-30 08:32:42 -0600

    add pcg vector generation support

--- L2/src/sw/pcg/python/gen_vectors.py
+++ L2/src/sw/pcg/python/gen_vectors.py
@@ -40,10 +40,15 @@ def write_vec(p_vec, p_vecFileName):
     p_vec.tofile(fo)
     fo.close()
 
-def gen_vectors(spm, mtxName, vecPath, parEntries):
-    l_inVecFileName = vecPath+'/'+mtxName+'/inVec.dat'
-    l_refVecFileName = vecPath+'/'+mtxName+'/refVec.dat'
-    l_diagMatFileName = vecPath+'/'+mtxName+'/mat_diag.dat'
+def gen_vectors(spm, isPcg, mtxName, vecPath, parEntries):
+    if isPcg:
+        l_inVecFileName = vecPath+'/'+mtxName+'/x.mat'
+        l_refVecFileName = vecPath+'/'+mtxName+'/b.mat'
+        l_diagMatFileName = vecPath+'/'+mtxName+'/A_diag.mat'
+    else:
+        l_inVecFileName = vecPath+'/'+mtxName+'/inVec.dat'
+        l_refVecFileName = vecPath+'/'+mtxName+'/refVec.dat'
+        l_diagMatFileName = vecPath+'/'+mtxName+'/mat_diag.dat'
     
     l_nPad = ((spm.n+parEntries-1)//parEntries) * parEntries
     l_mPad = ((spm.m+parEntries-1)//parEntries) * parEntries
@@ -61,7 +66,7 @@ def gen_vectors(spm, mtxName, vecPath, parEntries):
     write_vec(l_inVec, l_inVecFileName)
     write_vec(l_refVec, l_refVecFileName)
 
-def process_matrices(isGenVec, isClean, mtxList, parEntries, vecPath):
+def process_matrices(isGenVec, isPcg, isClean, mtxList, parEntries, vecPath):
     downloadList = open(mtxList, 'r')
     downloadNames = downloadList.readlines()
     if not path.exists(vecPath):
@@ -84,7 +89,7 @@ def process_matrices(isGenVec, isClean, mtxList, parEntries, vecPath):
         if isGenVec:
             spm = sparse_matrix()
             spm.read_matrix(mtxFullName, mtxName)
-            gen_vectors(spm, mtxName, vecPath, parEntries)
+            gen_vectors(spm, isPcg, mtxName, vecPath, parEntries)
         if isClean:
             subprocess.run(["rm", "-rf", './mtx_files/'+mtxName+'/'])
     if isClean:
@@ -94,14 +99,15 @@ def process_matrices(isGenVec, isClean, mtxList, parEntries, vecPath):
 def main(args):
     if (args.usage):
         print('Usage example:')
-        print('python gen_vectors.py --gen_vec [--clean] --mtx_list ./test_matrices.txt --vec_path ./vec_dat')
+        print('python gen_vectors.py --gen_vec [--pcg] [--clean] --mtx_list ./test_matrices.txt --vec_path ./vec_dat')
     else:
-        process_matrices(args.gen_vec, args.clean, args.mtx_list, args.par_entries, args.vec_path)
+        process_matrices(args.gen_vec, args.pcg, args.clean, args.mtx_list, args.par_entries, args.vec_path)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='read sparse matrix file, generate input vector and golden reference vector for SpMv')
     parser.add_argument('--usage',action='store_true',help='print usage example')
     parser.add_argument('--gen_vec',action='store_true',help='generate input and output vectors for a set of sparse matrices')
+    parser.add_argument('--pcg',action='store_true',help='generate the vector files required by PCG solver')
     parser.add_argument('--clean',action='store_true',help='clean up downloaded .mtx file after the run')
     parser.add_argument('--mtx_list',type=str,help='a file containing URLs for downloading sprase matrices')
     parser.add_argument('--par_entries',type=int,default=4,help='number of NNZ entries retrieved from one HBM channel')
