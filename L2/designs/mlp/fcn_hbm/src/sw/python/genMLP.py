5659171b269b696e024f0da0260752f9f9628663
Liang <liangm@xilinx.com>
2021-05-06 06:43:03 -0700

    add genMLP

--- L2/designs/mlp/fcn_hbm/src/sw/python/genMLP.py
+++ L2/designs/mlp/fcn_hbm/src/sw/python/genMLP.py
@@ -19,40 +19,25 @@ import sys
 import os
 import argparse
 import pdb
+from genFcn import *
 
 
-def linear(x, a=1, b=0):
-    return a * x + b
-
-
-def relu(x):
-    x[x < 0] = 0
-    return x
-
-
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
-
-class FCN:
-    def __init__(self, inS, outS, act=None, dtype=np.float32):
-        self.inS = inS
-        self.outS = outS
-        self.act = act
-        self.dtype = dtype
-        self.W = 2 * np.random.random((outS, inS)
-                                      ).astype(dtype=dtype) - 1
-        self.b = np.random.random(outS).astype(dtype=dtype) * 2 - 1
+class MLP:
+    def __init__(self, layers, act, dtype):
+        self.n = len(layers) - 1
+        self.layers = list()
+        for i in range(self.n):
+            self.layers.append(FCN(layers[i], layers[i + 1], act, dtype))
+        self.layers[-1].act = None
 
     def __call__(self, x):
-        y = x @ self.W.transpose() + self.b
-        if self.act is not None:
-            y = self.act(y)
-        return y
+        for fcn in self.layers:
+            x = fcn(x)
+        return x
 
-    def tofile(self, path, name='0'):
-        self.W.tofile(os.path.join(path, 'W_%s.mat' % name))
-        self.b.tofile(os.path.join(path, 'b_%s.mat' % name))
+    def tofile(self, path):
+        for i in range(self.n):
+            self.layers[i].tofile(path, i)
 
 
 def main(args):
@@ -63,21 +48,27 @@ def main(args):
     else:
         sys.exit("Wrong data type received.")
 
-    fcn = FCN(args.inSize, args.outSize, eval(args.act), dtype)
-    x = np.random.random((args.batch, args.inSize)).astype(dtype=dtype) * 2 - 1
+    mlp = MLP(args.layers, eval(args.act), dtype)
+    mlp.tofile(args.path)
+
+    x = np.random.random(
+        (args.batch, args.layers[0])).astype(
+        dtype=dtype) * 2 - 1
+    x.tofile(os.path.join(args.path, 'in.mat'))
+
     start = time.time_ns()
-    y = fcn(x)
+
+    y = mlp(x)
+
     stop = time.time_ns()
     print("Python execution time: %f seconds." % ((stop - start) * 1e-9))
 
-    fcn.tofile(args.path)
-    x.tofile(os.path.join(args.path, 'in.mat'))
     y.tofile(os.path.join(args.path, 'out.mat'))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='Generate FCN.')
+        description='Generate MLP.')
     parser.add_argument(
         '--path',
         type=str,
@@ -94,15 +85,10 @@ if __name__ == "__main__":
         default=2,
         help='Input batch')
     parser.add_argument(
-        '--outSize',
+        '--layers',
         type=int,
-        default=32,
-        help='Output vector dim')
-    parser.add_argument(
-        '--inSize',
-        type=int,
-        default=32,
-        help='Input vector dim')
+        nargs='+',
+        help='layer sizes')
     parser.add_argument(
         '--datatype',
         type=str,
