99a0539a8d62b1c4131a5021807c3448441cf865
Lisa Liu <lingl@xilinx.com>
2021-05-27 04:26:39 -0700

    starting re-org repo, removing files not in the first product line.

--- L3/benchmarks/mlp/cpu_c/benchmark.sh
+++
@@ -1,3 +0,0 @@
-make benchmark NUM_THREADS=96 | tee log
-egrep -h ^DATA_CSV log |  head -1  > perf_QRes_20NN_Single.csv
-egrep -h ^DATA_CSV log |  grep -v Type >> perf_QRes_20NN_Single.csv
--- L3/benchmarks/mlp/cpu_c/data/README.txt
+++
@@ -1,1 +0,0 @@
-please copy NN356inputs.bin, NN356.bin, NN356outputs.bin to this folder
