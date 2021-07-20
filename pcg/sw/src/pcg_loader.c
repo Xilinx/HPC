c94a30dec7b0015db237e8173e2421d20ed4a78a
Dave Liddell <dliddell@xilinx.com>
2021-07-19 19:06:00 -0600

    Dynamic loader and test now working

---
+++ pcg/sw/src/pcg_loader.c
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2020-2021N Xilinx, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Thanks to Aaron Isotton for his dynamic loading ideas in https://tldp.org/HOWTO/pdf/C++-dlopen.pdf
+
+#include "pcg.h"
+#include <dlfcn.h>
+#include <stdio.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef XILINX_PCG_INLINE_IMPL
+#define XILINX_PCG_LINKAGE_DEF inline
+#else
+#define XILINX_PCG_LINKAGE_DEF
+#endif
+    
+#define XILINX_PCG_SOFILENAME "libXilinxPcgStatic.so"
+
+XILINX_PCG_LINKAGE_DEF
+void *xilinx_apps_getCDynamicFunction(const char *funcName) {
+    // open the library
+    
+    void* handle = dlopen(XILINX_PCG_SOFILENAME, RTLD_LAZY | RTLD_NOLOAD);
+    if (!handle) {
+        printf("INFO: %s not loaded. Loading now...\n", XILINX_PCG_SOFILENAME);
+        handle = dlopen(XILINX_PCG_SOFILENAME, RTLD_LAZY | RTLD_GLOBAL);
+        if (!handle) {
+            printf("ERROR: Cannot open library %s: %s."
+                "  Please ensure that the library's path is in LD_LIBRARY_PATH.\n", XILINX_PCG_SOFILENAME, dlerror());
+            return 0;
+        }
+    }
+
+    // load the symbol
+    dlerror();  // reset errors
+    void *pFunc = dlsym(handle, funcName);
+    const char* dlsym_error2 = dlerror();
+    if (dlsym_error2) {
+        printf("ERROR: Cannot load symbol '%s': %s.  Possibly an older version of library %s"
+                " is in use.  Please install the correct version.\n", funcName, dlsym_error2, XILINX_PCG_SOFILENAME);
+        return 0;
+    }
+    return pFunc;
+}
+
+//#####################################################################################################################
+
+XILINX_PCG_LINKAGE_DEF
+void *create_JPCG_handle(int deviceId, const char *xclbinPath) {
+    typedef void *(*CreateFunc)(int, const char *);
+    CreateFunc pCreateFunc = (CreateFunc) xilinx_apps_getCDynamicFunction("create_JPCG_handle");
+    if (!pCreateFunc)
+        return 0;
+    return pCreateFunc(deviceId, xclbinPath);
+}
+
+XILINX_PCG_LINKAGE_DEF
+void destroy_JPCG_handle(void *handle) {
+    typedef void (*DestroyFunc)(void *);
+    DestroyFunc pDestroyFunc = (DestroyFunc) xilinx_apps_getCDynamicFunction("destroy_JPCG_handle");
+    if (pDestroyFunc)
+        pDestroyFunc(handle);
+}
+
+XILINX_PCG_LINKAGE_DEF
+void JPCG_coo(void *handle, JPCG_Mode mode, int dummy) {
+    typedef void (*ApiFunc)(void *, JPCG_Mode, int);
+    ApiFunc pApiFunc = (ApiFunc) xilinx_apps_getCDynamicFunction("JPCG_coo");
+    if (!pApiFunc)
+        return;
+    pApiFunc(handle, mode, dummy);
+}
+
+#ifdef __cplusplus
+}
+#endif
+
